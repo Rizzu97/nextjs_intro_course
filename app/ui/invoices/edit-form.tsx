5123e1f450609437eb5b0dbe60cb01627954c1b5
Pierluigi Rizzu <pierluigirizzu.it>
2024-04-28 14:23:29 +0200

    (done): finished intro project)

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -17,8 +17,16 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  function updateInvoice(
+    id: any,
+  ): string | ((formData: FormData) => void) | undefined {
+    throw new Error('Function not implemented.');
+  }
+
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
